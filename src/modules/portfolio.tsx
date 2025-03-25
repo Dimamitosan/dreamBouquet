7dedbe0d70fa5ee39fddede87ada05ab9fb5c893
Dimamitosan <shagarov07@gmail.com>
2025-03-25 16:53:17 +0300

    portfolio finished

--- src/modules/portfolio.tsx
+++ src/modules/portfolio.tsx
@@ -1,12 +1,29 @@
 import styles from './styles/portfolio.module.css'
 
+import picture from '../pictures/41919.jpg'
+
 function Portfolio() {
   return (
     <div className={styles.portfolio__container}>
-      <div className={styles.portfolio_text}>portfolio_text</div>
+      <div className={styles.portfolio_text}>Our portfolio</div>
       <div className={styles.block}>
-        <div className={styles.portfolio_buttons}>portfolio_buttons</div>
-        <div className={styles.portfolio_photos}>portfolio_photos</div>
+        <div className={styles.portfolio_buttons}>
+          <button className={styles.button}>All Categories</button>
+          <div className={styles.pairOfButtons}>
+            <button className={styles.button}>All Categories</button>
+            <button className={styles.button}>All Categories</button>
+          </div>
+
+          <button className={styles.button}>All Categories</button>
+        </div>
+        <div className={styles.portfolio_photos}>
+          <img src={picture} alt="" className={styles.responsive_image} />
+          <img src={picture} alt="" className={styles.responsive_image} />
+          <img src={picture} alt="" className={styles.responsive_image} />
+          <img src={picture} alt="" className={styles.responsive_image} />
+          <img src={picture} alt="" className={styles.responsive_image} />
+          <img src={picture} alt="" className={styles.responsive_image} />
+        </div>
       </div>
     </div>
   )
